97404684207ac84f138b7e967fa8f53971cc6d95
Thanh Ngo <>
2025-02-15 15:25:37 +0700

    add more vcard template

--- lib/screens/vcard_screen/vcard_template_widget.dart
+++ lib/screens/vcard_screen/vcard_template_widget.dart
@@ -11,7 +11,39 @@ class VcardTemplateWidget extends StatefulWidget {
 }
 
 class _VcardTemplateWidgetState extends State<VcardTemplateWidget> {
-  final list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  final list = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -38,8 +70,8 @@ class _VcardTemplateWidgetState extends State<VcardTemplateWidget> {
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(8),
                           child: Image.asset(
-                            'assets/images/vcard$e.webp',
-                            fit: BoxFit.fitHeight,
+                            'assets/templates/vcard$e.png',
+                            fit: BoxFit.cover,
                           ),
                         ),
                       ),
@@ -54,51 +86,6 @@ class _VcardTemplateWidgetState extends State<VcardTemplateWidget> {
                   ],
                 ))
             .toList(),
-      
-        //  [
-        // Expanded(
-        //     child: ListView.builder(
-        //   padding: const EdgeInsets.symmetric(horizontal: 16),
-        //   itemCount: list.length,
-        //   itemBuilder: (context, index) {
-        //     return Column(
-        //       children: [
-        //         GestureDetector(
-        //           onTap: () {
-        //             setState(() {
-        //               widget.model.selectedTemplateID = list[index];
-        //             });
-        //           },
-        //           child: Card(
-        //             elevation: 2,
-        //             borderOnForeground: true,
-        //             shape: RoundedRectangleBorder(
-        //               side: widget.model.selectedTemplateID == list[index]
-        //                   ? const BorderSide(color: Colors.green, width: 3)
-        //                   : BorderSide.none,
-        //               borderRadius: BorderRadius.circular(10),
-        //             ),
-        //             child: ClipRRect(
-        //               borderRadius: BorderRadius.circular(8),
-        //               child: Image.asset(
-        //                 'assets/images/vcard${list[index]}.webp',
-        //                 fit: BoxFit.fitHeight,
-        //               ),
-        //             ),
-        //           ),
-        //         ),
-        //         const Padding(
-        //           padding: EdgeInsets.all(8),
-        //           child: Divider(
-        //             height: 2,
-        //             color: Colors.white,
-        //           ),
-        //         )
-        //       ],
-        //     );
-        //   },
-        // ))
-        // ],
       ),
     );
   }
